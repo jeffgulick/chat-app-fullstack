b2b5e52acf98aefb05046a2249251e70d065c131
JEFFGULICK <jeffgulick@gmail.com>
2021-03-26 00:45:40 -0500

    troubleshooting

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
-const path = require('path');
 require('dotenv').config();
+const path = require('path');
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
