9a8f8ddf33308f612505dab8d3e20863605d36ec
JEFFGULICK <jeffgulick@gmail.com>
2021-03-25 23:20:57 -0500

    test

--- server/index.js
+++ server/index.js
@@ -57,12 +57,11 @@ io.on("connection", socket => {
   });
 });
 
-if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
-}
+
 
 const port = process.env.PORT || 3001;
 server.listen(port, () => {
