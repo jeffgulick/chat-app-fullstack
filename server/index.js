d72d07a9cb479737db5c77889c7cb92524886f76
JEFFGULICK <jeffgulick@gmail.com>
2021-03-26 19:17:48 -0500

    status

--- server/index.js
+++ server/index.js
@@ -66,8 +66,8 @@ app.get('*', (req, res) => {
 
   app.enable("trust proxy");
 
-    // app.use(express.static("app/client/build"));
-  // app.get("*", (req, res) => {
+    // app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 
