8f9155ee498ca1493fdb80de90c42e42c2a020ee
JEFFGULICK <jeffgulick@gmail.com>
2021-03-26 19:40:12 -0500

    commit

--- server/index.js
+++ server/index.js
@@ -57,22 +57,18 @@ io.on("connection", socket => {
   });
 });
 
-app.use(express.static(path.join(__dirname, 'client/build')))
+// app.use(express.static(path.join(__dirname, 'client/build')))
 app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname+ '/client/build/index.html'))
-// })
-
-
-
-  app.enable("trust proxy");
-
-    // app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+  res.sendFile(path.join(__dirname+ '/client/build/index.html'))
+})
+app.enable("trust proxy");
+app.use(express.static("client/build"));
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  // });
 
 
 const port = process.env.PORT || 3001;
 server.listen(port, () => {
   console.log(`Server Listening on ${port}`);
-});
+})
