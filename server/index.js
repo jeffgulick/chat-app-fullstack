b55789efaf3a92402cdaf5d00e610efdda465029
JEFFGULICK <jeffgulick@gmail.com>
2021-03-28 13:34:46 -0500

    removed commented code

--- server/index.js
+++ server/index.js
@@ -57,16 +57,11 @@ io.on("connection", socket => {
   });
 });
 
-// app.use(express.static(path.join(__dirname, 'client/build')))
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+ '/client/build/index.html'))
 })
 app.enable("trust proxy");
 app.use(express.static("client/build"));
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  // });
-
 
 const port = process.env.PORT || 3001;
 server.listen(port, () => {
